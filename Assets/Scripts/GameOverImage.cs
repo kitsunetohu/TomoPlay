688e4656f128348bb5b24cb85d81e5125340389f
AkioMabuchi <mabuchiakio@gmail.com>
2019-07-06 20:44:09 +0900

    Created TitleScene

--- Assets/Scripts/GameOverImage.cs
+++ Assets/Scripts/GameOverImage.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class GameOverImage : MonoBehaviour
 {
+
+    GameOverManager gameOverManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOverManager = GameObject.Find("GameOverManager").GetComponent<GameOverManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadTitleScene()
+    {
+        gameOverManager.LoadTitleScene();
     }
 }
