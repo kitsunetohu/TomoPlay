1825ff5442b1401809168e2257bbc9a345542851
fppz8391 <fppz8391@gmail.com>
2019-07-07 04:58:41 +0900

    miss

---
+++ Assets/Scripts/CharacterSeparateMove.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniP2P.HLAPI;
+using DG.Tweening;
+
+public class CharacterSeparateMove : MonoBehaviour
+{
+	static int hashSpeed = Animator.StringToHash ("Speed");
+	static int hashFallSpeed = Animator.StringToHash ("FallSpeed");
+	static int hashGroundDistance = Animator.StringToHash ("GroundDistance");
+	static int hashIsCrouch = Animator.StringToHash ("IsCrouch");
+
+	static int hashDamage = Animator.StringToHash ("Damage");
+
+	[SerializeField] private float characterHeightOffset = 0.2f;
+	[SerializeField] LayerMask groundMask;
+
+	[SerializeField, HideInInspector] Animator animator;
+	[SerializeField, HideInInspector]SpriteRenderer spriteRenderer;
+	[SerializeField, HideInInspector]Rigidbody2D rig2d;
+
+    private SyncGameObject SyncGameObject;
+
+
+    void Awake ()
+	{
+		animator = GetComponent<Animator> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		rig2d = GetComponent<Rigidbody2D> ();
+        SyncGameObject = GetComponent<SyncGameObject>();
+    }
+
+    void Update()
+    {
+        float axisX, axisY;
+        if (SyncGameObject.IsMine) { 
+            axisX = Input.GetAxisRaw("Horizontal");
+            axisY = Input.GetAxisRaw("Vertical");
+        }	
+
+        Vector2 velocity = rig2d.velocity;
+		if (axisX != 0||axisY!=0){
+			spriteRenderer.flipX = axisX < 0;
+            velocity.x = axisX * 2;
+			velocity.y=axisY*2;
+        }
+		else
+		{
+			velocity.x=velocity.y=0;
+		}
+		if(GameManger.Instance.userCanMove){
+			rig2d.velocity = velocity;
+		}
+	
+		animator.SetFloat (hashSpeed, Mathf.Abs (velocity.magnitude));
+
+	}
+    
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        animator.SetTrigger(hashDamage);  
+    }
+
+	public void moveToByAnima(Vector3 target,float t){
+		GameManger.Instance.userCanMove=false;
+		transform.DOMove(target,t).OnComplete(()=>{GameManger.Instance.userCanMove=true;});
+	}
+}
